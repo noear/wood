7b32fd658f930561a88bb54bc17aa37317a6930a
noear <noear@live.cn>
2024-09-05 09:31:40 +0800

    1.3.3

--- _test/src/test/java/wood_rdb/features/_MetaTest.java
+++ _test/src/test/java/wood_rdb/features/_MetaTest.java
@@ -8,18 +8,50 @@ public class _MetaTest {
     DbContext db = DbUtil.db;
 
     @Test
-    public void test1() throws Exception{
+    public void test1() throws Exception {
 
-        db.getMetaData().getTableAll().forEach(tw->{
-            System.out.println("Table: "+tw.getName());
-            tw.getColumns().forEach(cw->{
-                System.out.print(cw.getName()+";");
+        db.getMetaData().getTableAll().forEach(tw -> {
+            System.out.println("Table: " + tw.getName());
+            tw.getColumns().forEach(cw -> {
+                System.out.print(cw.getName() + ";");
             });
             System.out.println("");
         });
 
         System.out.println(db.getMetaData().getTableAll().size());
 
-        assert  db.getMetaData().getTableAll().size() > 0;
+        assert db.getMetaData().getTableAll().size() > 0;
+    }
+
+    @Test
+    public void test2() throws Exception {
+
+        db.getMetaData().getTableAll().forEach(tw -> {
+            System.out.println("Table: " + tw.getName());
+            tw.getColumns().forEach(cw -> {
+                System.out.print(cw.getName() + ";");
+            });
+            System.out.println("");
+        });
+
+        System.out.println(db.getMetaData().getTableAll().size());
+
+        assert db.getMetaData().getTableAll().size() > 0;
+
+
+        //刷新表
+        db.getMetaData().refreshTables();
+
+        db.getMetaData().getTableAll().forEach(tw -> {
+            System.out.println("Table2: " + tw.getName());
+            tw.getColumns().forEach(cw -> {
+                System.out.print(cw.getName() + ";");
+            });
+            System.out.println("");
+        });
+
+        System.out.println(db.getMetaData().getTableAll().size());
+
+        assert db.getMetaData().getTableAll().size() > 0;
     }
 }
