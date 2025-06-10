8ba94a0e7334c118581ec50c53cdbf35a0d823fe
songyinyin <song_yinyin@163.com>
2025-06-10 15:52:50 +0800

    feat: 支持 OceanBase

---
+++ wood/src/main/java/org/noear/wood/dialect/DbOceanBaseMySQLDialect.java
@@ -0,0 +1,10 @@
+package org.noear.wood.dialect;
+
+/**
+ * OceanBase MySQL 模式
+ *
+ * @author songyinyin
+ * @since 2025/6/10 14:40
+ */
+public class DbOceanBaseMySQLDialect extends DbMySQLDialect {
+}
