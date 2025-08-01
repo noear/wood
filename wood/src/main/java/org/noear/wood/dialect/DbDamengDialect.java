a91e1a8abf8855a58d39d2681b83178dd581b5bf
songyinyin <song_yinyin@163.com>
2025-08-01 16:08:10 +0800

    fix: dameng 数据库行为和 oracle 保持一致

--- wood/src/main/java/org/noear/wood/dialect/DbDamengDialect.java
+++ wood/src/main/java/org/noear/wood/dialect/DbDamengDialect.java
@@ -8,29 +8,4 @@ package org.noear.wood.dialect;
  */
 public class DbDamengDialect extends DbOracleDialect {
 
-    @Override
-    public String tableFormat(String tb) {
-        String[] ss = tb.split("\\.");
-
-        if (ss.length > 1) {
-            return "\"" + ss[0] + "\".\"" + ss[1] + "\"";
-        } else {
-            return "\"" + ss[0] + "\"";
-        }
-    }
-
-    @Override
-    public String columnFormat(String col) {
-        String[] ss = col.split("\\.");
-
-        if (ss.length > 1) {
-            if ("*".equals(ss[1])) {
-                return "\"" + ss[0] + "\".*";
-            } else {
-                return "\"" + ss[0] + "\".\"" + ss[1] + "\"";
-            }
-        } else {
-            return "\"" + ss[0] + "\"";
-        }
-    }
 }
